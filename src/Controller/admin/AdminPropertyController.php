56e912415b355077aeb165e54e2d01b70e340fae
dimebag <benmounah.mohcen@gmail.com>
2019-02-10 21:17:17 +0100

    creation formulaire et edition

--- src/Controller/admin/AdminPropertyController.php
+++ src/Controller/admin/AdminPropertyController.php
@@ -1,27 +1,64 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\admin;
 
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use App\Repository\PropertyRepository;
+use App\Entity\Property;
+use App\Form\PropertyType;
+use Symfony\Component\HttpFoundation\Tests\RequestContentProxy;
 
 class AdminPropertyController extends AbstractController {
 
     private $repository;
 
 
-    public function __construct(PropertyRepository $repository)
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
     {
-            $this->repository = $repository;compac
+            $this->repository = $repository;
+            $this->em = $em;
     }
 
     public function index() {
         $properties = $this->repository->findAll();
-        return $this->render('admin/property/index.html.twig', compacte('properties'));
+        return $this->render('admin/property/index.html.twig', compact('properties'));
 
     }
+
+    public function new(Request $request) {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        dump("test");
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist();
+            $this->em->flush();
+            return $this->redirectToRoute("admin.property.index");
+        }
+        return $this->render('admin/property/new.html.twig', ["property" => $property, "form"=>$form->createView()]);
+
+    }
+
+    public function edit(Property $property, Request $request) {
+
+       $form = $this->createForm(PropertyType::class, $property); // creation du formulaire
+        //Traitement du post formulaire
+        $form->handleRequest($request);//test s'il y a du changement au niveau des données
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();// no persist because update
+            return $this->redirectToRoute("admin.property.index");
+        }
+       return $this->render('admin/property/edit.html.twig', ["property" => $property, "form"=>$form->createView()]);
+
+
+    }
+
+
 
 
 
